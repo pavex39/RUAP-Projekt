f774bf0608aa1ddd330bcf78fb2937082a2fe19c
fury565 <legotoster@gmail.com>
2022-11-03 17:40:08 +0100

    gradnadravi

---
+++ KonzolnaAplikacijaPetar/KonzolnaAplikacijaPetar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KonzolnaAplikacijaPetar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Petra Pavica");
+        }
+    }
+}
